9c122b773b62d0b32d127653c03ec9a7fc752e21
@Gabrielll04 <gabrielggcarneiro04@gmail.com>
2023-05-01 01:20:36 -0300

    Adding vscode

--- home.nix
+++ home.nix
@@ -3,6 +3,7 @@
 let 
     customNeovim = import ./config/nvim/nvim.nix; 
     customGit = import ./config/git/git.nix;
+    customVscode = import ./config/vscode/vscode.nix;
 in
 {
     home.username = "guimaraes";
@@ -23,6 +24,7 @@ in
 
     programs.home-manager.enable = true;
 
+    programs.vscode = customVscode pkgs;
     programs.neovim = customNeovim pkgs;
     programs.git = customGit pkgs;
 }
