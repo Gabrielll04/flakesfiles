462e3bc54f7a6c7a3404bd194773c0bdf79962ec
@Gabrielll04 <gabrielggcarneiro04@gmail.com>
2023-08-16 16:48:17 -0300

    Renaming home.nix and configuration.nix to default.nix

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
-        modules = [ ./nixos/configuration.nix ];
+        modules = [ ./nixos ];
       };
     };
 
@@ -21,7 +21,7 @@
       "guimaraes@nixos" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         extraSpecialArgs = { inherit inputs; };
-        modules = [ ./home-manager/home.nix ];
+        modules = [ ./home-manager ];
       };
     };
   };
